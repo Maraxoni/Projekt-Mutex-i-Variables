55afb5a1ea520ca46791687f1bd823895ac2ed32
Maraxoni <103191241+Maraxoni@users.noreply.github.com>
2023-06-17 22:13:21 +0200

    Final ProjektVariables.c

--- ProjektVariables.c
+++ ProjektVariables.c
@@ -4,188 +4,239 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
-//zdefiniowanie stalej wartosci maksymalnej liczby samochodow
+#include <semaphore.h>
+#include <time.h>
+//zdefiniowanie maksymalnej wartosci watkow
 #define MAX 100
-//zmienna okreslajaca czy maja sie wyswietlac zawartosci kolejek
+//deklaracja zmiennych
 int info = false;
-int onBridge = 0;
-//inicjalizacja statycznego mutexa
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-//deklaracja struktury wezla kolejki
+volatile int waitingA = 0;
+volatile int waitingB = 0;
+//inicjalizacja mutexow i condition variables
+pthread_mutex_t mutexCity = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cityCondition = PTHREAD_COND_INITIALIZER;
+//deklaracja struktury Queue
 typedef struct Queue {
-    pthread_t thread;
+    int id;
+    char city;
     struct Queue* next;
 } Queue;
-//dodawanie nowego elementu kolejki
-void insertQueue(Queue** head, const char* car) {
-    //stworzenie nowego wezla
-    Queue* newNode = malloc(sizeof(Queue));
-    //przypisanie zmiennej nameCar zawartosci podanej na wejscie zmiennej car
-    strncpy(newNode->nameCar, car, MAX - 1);
-    //przypisanie \0 na koniec listy znakow i przypisanie kolejnemu wezlowi wartosci NULL
-    newNode->nameCar[MAX - 1] = '\0';
-    newNode->next = NULL;
-    //sprawdzenie czy kolejka jest pusta
+
+Queue* queue = NULL; //stworzenie globalnej kolejki queue w oparciu o strukture
+//funkcja dodajaca 
+void insertQueue(Queue** head, Queue* newNode) {
+    if (newNode == NULL) {
+        printf("Trying to add NULL\n");
+        return;
+    }
+
+    newNode->next = NULL; // ustawienie wskaznika elementu nastepnego na NULL
+    //sprawdzenie czy pierwszy element kolejki istnieje i w przeciwnym razie stworzenie nowego
     if (*head == NULL) {
         *head = newNode;
-    }
-    else {
+    } else {
         Queue* current = *head;
-        //przeiterowanie na koniec kolejki
+        //petla przechodzoca na koniec kolejki
         while (current->next != NULL) {
             current = current->next;
         }
-        //wstawienie do kolejki wezla newNode na ostatniej pozycji
         current->next = newNode;
     }
+    
 }
-//usuwanie pierwszego elementu kolejki
+//funkcja usuwajaca pierwszy element i ustawiajacy element nastepny jako pierwszy
 void popQueue(Queue** head) {
-    //sprawdzanie czy kolejka jest pusta
     if (*head == NULL) {
-        printf("Error: The queue is emptyP.\n");
+        printf("Error: The queue is empty.\n");
         return;
     }
-    //usuwanie przez przypisanie headowi wezla nastepnego
     Queue* temp = *head;
     *head = (*head)->next;
     free(temp);
 }
-//zwracanie nazwy
-char* topQueue(Queue* head) {
-    //sprawdzanie czy kolejka jest pusta
+//funkcja zwracajaca id pierwszego elementu w kolejce
+int topQueue(Queue* head) {
     if (head == NULL) {
-        printf("Error: The queue is emptyT.\n");
-        return NULL;
+        printf("Error: The queue is empty.\n");
+        return 0;
     }
-    //zwracanie nazwy pierwszej pozycji w kolejce
-    return head->thread;
+    int id = head->id;
+    return id;
 }
-//wypisywanie zawartosci kolejki
+//funkcja wypisujaca zawartosc kolejki watkow
 void printQueue(Queue* head) {
     printf("Queue:\n");
-    //sprawdzenie czy kolejka jest pusta
+    //sprawdzanie czy kolejka jest pusta
     if (head == NULL) {
         printf("Empty.\n\n");
         return;
-    }
-    else {
+    } else {
         Queue* current = head;
         int i = 1;
-        //iteracja po wezlach az do konca kolejki wypisujac ich zawartosc
+        //iteracja po kolejce i wypisywanie id elementow oraz miast w ktorych jest watek
         while (current != NULL) {
-            printf("%d: %s\n", i, current->thread);
+            printf("%d: %d,%c\n", i, current->id, current->city);
+            //ustawianie wskaznika obecnego elementu na wskaznik nastepnego
             current = current->next;
             i++;
         }
     }
     printf("\n");
-    return;
 }
-//okreslenie wielkosci kolejki
-int sizeQueue(Queue* head) {
+//funkcja tworzaca dwie kolejki w oparciu o queue i miasta w ktorych sa watki i wypisujaca ich zawartosc
+void printQueues(Queue* head) {
+    if (head == NULL) {
+        printf("Both queues empty.\n\n");
+        return;
+    } else {
+        //tworzenie kolejek
+        Queue* queueA = NULL;
+        Queue* queueB = NULL;
+        Queue* current = head;
+        while (current != NULL) {
+            Queue* temp = current; // tworzenie tymczasowego wskaznika
+            if (current->city == 'A') {
+                Queue* newNode = malloc(sizeof(Queue));
+                newNode->id = current->id;
+                newNode->city = current->city;
+                insertQueue(&queueA, newNode);
+            } else if (current->city == 'B') {
+                Queue* newNode = malloc(sizeof(Queue));
+                newNode->id = current->id;
+                newNode->city = current->city;
+                insertQueue(&queueB, newNode);
+            }
+
+            current = temp->next; // przechodzenie do nastepnego wezla
+        }
+        //wypisywanie kolejek
+        printf("Queue A:\n");
+        printQueue(queueA);
+        printf("Queue B:\n");
+        printQueue(queueB);
+    }
+    printf("\n");
+}
+//funkcja zwracajaca wielkosc kolejki w zaleznosci od miasta podanego jako argument
+int sizeQueue(Queue* head, char check) {
     int size = 0;
     Queue* current = head;
-    //zliczanie ilosci wezlow
     while (current != NULL) {
-        size++;
+        if (current->city == check) {
+            size++;
+        }
         current = current->next;
     }
     return size;
 }
-//losowe obliczenia
-void cityWait()
-{
-    for (int i = 0;i < 1000000000;i++) {
-        int b = i + 1;
+//wykonywanie obliczen przez losowy czas
+void cityWait() {
+    for (int i = 0; i < 9999999999 * (rand() % 5); i++) {
+        for (int j = 0; j < 9999999; j++) {
+            int b = i + 1;
+        }
     }
 }
-//watek miasta A pozwalajacy przemieszczenie sie pojazdu z kolejki queueA do kolejki queueB
+//funkcja watku zmieniajaca swoj stan z A na B i z powrotem
 void* city(void* arg) {
-    int id = threadID;
-    char whichCity = 'A';
-    //nieskonczona petla watku
+    //przypisanie watkowi id oraz miasta podanego w argumencie
+    Queue* data = (Queue*)arg;
+    int idThread = data->id;
+    char cityThread = data->city;
     while (1) {
+        //wykonywanie obliczen zajmujacych czas
         cityWait();
-        pthread_mutex_lock(&mutex); //zablokowanie mutexa
-        //sprawdzenie czy kolejka A jest pusta
-        pritnf(">>");
-        if (whichCity = 'A'){
-            whichCity = B;
+        //zablokowanie mutexu 
+        pthread_mutex_lock(&mutexCity);
+        Queue* temp = malloc(sizeof(Queue));
+        temp->id = idThread;
+        temp->city = cityThread;
+        //dodanie watku do kolejki
+        insertQueue(&queue, temp);
+        //odblokowanie mutexu
+        pthread_mutex_unlock(&mutexCity);
+
+        //zablokowanie mutexu
+        pthread_mutex_lock(&mutexCity);
+        //oczekiwanie az nastapi kolej wykonania watku
+        while (topQueue(queue) != idThread) {
+            pthread_cond_wait(&cityCondition, &mutexCity);
         }
-        else {
-            whichCity = A;
+        //usuniecie watku z listy
+        popQueue(&queue);
+        //wykonanie zamiany miasta watku i wypisanie zawartosci kolejek
+        if (cityThread == 'A') {
+            waitingA--; //zmniejszenie liczby samochodow znajdujacych sie w miescie A
+            printf("CityA-%d QueueA-%d --> [>> %d >>] <-- QueueB-%d CityB-%d\n", waitingA-sizeQueue(queue, 'A'), sizeQueue(queue, 'A'), idThread, sizeQueue(queue, 'B'), waitingB-sizeQueue(queue, 'B'));
+            cityThread = 'B';
+            temp->city = 'B';
+            waitingB++; //zwiekszenie liczby samochodow znajdujacych sie w miescie B
+        } else {
+            waitingB--; //zmniejszenie liczby samochodow znajdujacych sie w miescie B
+            printf("CityA-%d QueueA-%d --> [<< %d <<] <-- QueueB-%d CityB-%d\n", waitingA-sizeQueue(queue, 'A'), sizeQueue(queue, 'A'), idThread, sizeQueue(queue, 'B'), waitingB-sizeQueue(queue, 'B'));
+            cityThread = 'A';
+            temp->city = 'A';
+            waitingA++; //zwiekszenie liczby samochodow znajdujacych sie w miescie A
         }
-        
-        //sprawdzenie czy program jest odpalony z parametrem -info, potencjalne wypisanie zawartosci kolejek
-       /* if (info == true) {
+        if(info == 1){
             printf("---\n");
-            printQueue(*qA);
-            printQueue(*qB);
+            printQueues(queue);
         }
-        //wypisanie wartosci wedlug schematu
-        printf("CityA-%d QueueA-%d --> [>> %s >>] <-- QueueB-%d CityB-%d\n", count_CityA, sizeQueue(*qA), temp, sizeQueue(*qB), count_CityB);
-        printf("CityA-%d QueueA-%d --> [<< %s <<] <-- QueueB-%d CityB-%d\n", count_CityA, sizeQueue(*qA), temp, sizeQueue(*qB), count_CityB);
-        */
-        pthread_mutex_unlock(&mutex); //odblokowanie mutexa
+        //wyslanie sygnalu o zakonczeniu wykonania funkcji przez watek
+        pthread_cond_broadcast(&cityCondition);
+        //odblokowanie mutexu
+        pthread_mutex_unlock(&mutexCity);
     }
-    //wyjscie z watku
+
     pthread_exit(NULL);
 }
 
-//wyjscie z watku
-pthread_exit(NULL);
-}
-
-//main
 int main(int argc, char** argv) {
-    //domyslna ilosc samochodow
-    int cars = 5;
-    //sprawdzanie ilosci argumentow
+    //domyslna watosc watkow
+    int cars = 15;
+    srand(time(NULL));
+    //sprawdzenie ilosci argumentow
     if (argc < 1 || argc > 4) {
-        printf("Invaid arguments. Try -N [Number of cars] or -info\n");
+        printf("Invalid arguments. Try -N [Number of cars] or -info\n");
         return 1;
     }
-    //sprawdzanie typow argumentow i wykonywanie odpowiednich zadan w zaleznosci od argumentu
+    //sprawdzenie poprawnosci argumentow
     for (int i = 1; i < argc; i++) {
-        //sprawdzanie czy pojawil sie argument -N i przypisanie nowej wartosci dla ilosci samochodow
         if (strcmp(argv[i], "-N") == 0) {
-            i++;
+            i++; //ustawnienie na sprawdzanie nastepnego elementu czy jest intem wiekszym od 0
             if (argv[i] == NULL || atoi(argv[i]) <= 0) {
-                printf("Invalid number of cars. Insert value between 1 and %d\n", MAX);
+                printf("Invalid number of cars. Insert a value between 1 and %d\n", MAX);
                 return 1;
             }
             cars = atoi(argv[i]);
-        }
-        else if (strcmp(argv[i], "-info") == 0) { //sprawdzanie czy pojawil sie argument -info
-            info = 1;
-        }
-        else {
-            printf("Invalid argument: %s\n", argv[i]);
+        } else if (strcmp(argv[i], "-info") == 0) {
+            info = 1; //ustawienie wartosci info na 1
+        } else {
+            printf("Invalid argument: %s\n", argv[i]); //komunikat o zlym argumencie
             return 1;
         }
     }
-    //wypisanie wartosci zmiennych cars i info
+    //ustawienie ilosci samochodow w miescie A na ilosc samochodow
+    waitingA = cars;
+    //wypisanie nowych zmiennych
     fprintf(stdout, "info: %d\n", info);
     fprintf(stdout, "cars: %d\n", cars);
     printf("Starting Queue:\n");
-    //oddzielenie
     printf("-----\n");
-    //przypisanie pamieci watkom
-    pthread_t* tid = malloc(cars * sizeof(pthread_t));  // Alokacja pamieci
-
-    //stworzenie watkow
+    //deklaracja tid watku
+    pthread_t* tid = malloc(cars * sizeof(pthread_t));
+    //stworzenie watkow i przydzielenie im unikalnego id oraz miasta startowego A
     for (int i = 0; i < cars; i++) {
-        carID = i + 1;
-        pthread_create(&tid[i], NULL, city, carID);
+        Queue* data = malloc(sizeof(Queue));
+        data->id = i + 1;
+        data->city = 'A';
+        pthread_create(&tid[i], NULL, city, (void*)data);
     }
-    //oczekiwanie na zakonczenie watkow
+    //czekanie na wykonanie watkow
     for (int i = 0; i < cars; i++) {
         pthread_join(tid[i], NULL);
     }
-    //zwolnienie pamieci
+    //zwolnienie pamieci z tid
     free(tid);
-    free(queues);
-
     return 0;
 }
